84121ea53babdb9ca1f2e6fc9d981d842a5cd3fd
PHUNG NHU NGOC <1721050229@student.humg.edu.vn>
2021-12-21 13:08:38 +0700

    create_table_PNN229

---
+++ LTQL_1721050229/LTQL_1721050229/Models/NhaCungCap229.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace LTQL_1721050229.Models
+{
+    [Table("NhaCungCap")]
+    public class NhaCungCap229
+    {
+        [Key]
+        [Display(Name = "Mã Nhà Cung Cấp")]
+         public int MaNhaCungCap { get; set; }
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
+        [Display(Name = "Tên Nhà Cung Cấp ")]
+        [StringLength(50)]
+        public string TenNhaCungCap { get; set; }
+        }
+}
